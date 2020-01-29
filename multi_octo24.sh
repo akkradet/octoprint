7f8e73f1163ba92692d44872656db6d3a700f1fc
akkradet <akkradet7@gmail.com>
2020-01-29 11:12:10 +0700

    update 29/01/2020

--- multi_octo24.sh
+++ multi_octo24.sh
@@ -7,21 +7,21 @@ sed s/octoprint/octoprint3/ < octoprint | sed s/OctoPrint/OctoPrint3/ | sed s/bi
 chmod 755 octoprint3
 sed s/octoprint/octoprint4/ < octoprint | sed s/OctoPrint/OctoPrint4/ | sed s/bin\\/octoprint4/bin\\/octoprint/ > octoprint4
 chmod 755 octoprint4
-sed s/octoprint/octoprint5/ < octoprint | sed s/OctoPrint/OctoPrint5/ | sed s/bin\\/octoprint5/bin\\/octoprint/ > octoprint5
+sed s/octoprint/octoprint5/ < octoprint | sed s/OctoPrint/OctoPrint5/ | sed s/bin\\/octoprint2/bin\\/octoprint/ > octoprint5
 chmod 755 octoprint5
-sed s/octoprint/octoprint6/ < octoprint | sed s/OctoPrint/OctoPrint6/ | sed s/bin\\/octoprint6/bin\\/octoprint/ > octoprint6
+sed s/octoprint/octoprint6/ < octoprint | sed s/OctoPrint/OctoPrint6/ | sed s/bin\\/octoprint3/bin\\/octoprint/ > octoprint6
 chmod 755 octoprint6
-sed s/octoprint/octoprint7/ < octoprint | sed s/OctoPrint/OctoPrint7/ | sed s/bin\\/octoprint7/bin\\/octoprint/ > octoprint7
+sed s/octoprint/octoprint7/ < octoprint | sed s/OctoPrint/OctoPrint7/ | sed s/bin\\/octoprint4/bin\\/octoprint/ > octoprint7
 chmod 755 octoprint7
-sed s/octoprint/octoprint8/ < octoprint | sed s/OctoPrint/OctoPrint8/ | sed s/bin\\/octoprint8/bin\\/octoprint/ > octoprint8
+sed s/octoprint/octoprint8/ < octoprint | sed s/OctoPrint/OctoPrint8/ | sed s/bin\\/octoprint2/bin\\/octoprint/ > octoprint8
 chmod 755 octoprint8
-sed s/octoprint/octoprint9/ < octoprint | sed s/OctoPrint/OctoPrint9/ | sed s/bin\\/octoprint9/bin\\/octoprint/ > octoprint9
+sed s/octoprint/octoprint9/ < octoprint | sed s/OctoPrint/OctoPrint9/ | sed s/bin\\/octoprint3/bin\\/octoprint/ > octoprint9
 chmod 755 octoprint9
-sed s/octoprint/octoprint10/ < octoprint | sed s/OctoPrint/OctoPrint10/ | sed s/bin\\/octoprint10/bin\\/octoprint/ > octoprint10
+sed s/octoprint/octoprint10/ < octoprint | sed s/OctoPrint/OctoPrint10/ | sed s/bin\\/octoprint4/bin\\/octoprint/ > octoprint10
 chmod 755 octoprint10
-sed s/octoprint/octoprint11/ < octoprint | sed s/OctoPrint/OctoPrint11/ | sed s/bin\\/octoprint11/bin\\/octoprint/ > octoprint11
+sed s/octoprint/octoprint11/ < octoprint | sed s/OctoPrint/OctoPrint11/ | sed s/bin\\/octoprint4/bin\\/octoprint/ > octoprint11
 chmod 755 octoprint11
-sed s/octoprint/octoprint12/ < octoprint | sed s/OctoPrint/OctoPrint12/ | sed s/bin\\/octoprint12/bin\\/octoprint/ > octoprint12
+sed s/octoprint/octoprint12/ < octoprint | sed s/OctoPrint/OctoPrint12/ | sed s/bin\\/octoprint4/bin\\/octoprint/ > octoprint12
 chmod 755 octoprint12
 cd /etc/default
 sed s/PORT=5000/PORT=5001/ < octoprint | sed s/HOST=127.0.0.1/HOST=0.0.0.0/ | sed s/\$PORT/\$PORT\ --basedir\ \\/home\\/pi\\/.octoprint1/ > octoprint1
